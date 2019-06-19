eca9fb55e1b620e60106df9b73a59d11f554e8a3
Brian022 <38355408+Brian022@users.noreply.github.com>
2019-06-19 11:31:20 -0500

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class Stack;
+
+template<typename T>
+class Node
+{
+    private:
+    Node<T> *sgte;
+    T data;
+
+    public:
+    Node(T data){this->data = data;}
+
+    friend class Stack<T>;
+};
+
+template<typename T>
+class Stack
+{
+    private:
+    Node<T> *top;
+    Node<T> *actual;
+    public:
+
+    Stack()
+    {
+        top = NULL;
+    }
+    ~Stack()
+    {
+        while(!vacio())
+            pop();
+    }
+    bool vacio()
+    {
+        if(top == NULL)
+             return true;
+        return false;
+
+    }
+
+
+    void push(T data)
+    {
+        Node<T> *newTop = new Node<T>(data);
+        if(top==NULL)
+        {
+            top = newTop;
+        }
+        else
+        {
+            newTop->sgte=top;
+        }
+        top=newTop;
+
+    }
+
+    void pop()
+    {
+        if(!vacio())
+        {
+        Node<T> *actual =top;
+        top=actual->sgte;
+        delete actual;
+        return;
+        }
+    }
+
+    void mostrar()
+    {
+        Node<T> *actual = top;
+        while(actual != NULL)
+        {
+            cout<<actual->data<<endl;
+            actual = actual->sgte;
+        }
+    }
+
+};
+
+int main()
+{
+    Stack<int> a;
+
+    a.push(5);
+    a.push(9);
+    a.push(10);
+    a.push(8);
+    a.mostrar();
+    cout<<"-------";
+    a.pop();
+    a.pop();
+    a.pop();
+    a.pop();
+    a.mostrar();
+
+
+
+
+    return 0;
+}
